157bd9d174f79c7a84f6bbe391bde5e1c33e6956
Jean-Marie Comets <jean.marie.comets@gmail.com>
2014-11-01 18:21:17 +0100

    Implement BST thoroughly

--- bstrees.py
+++ bstrees.py
@@ -25,50 +25,108 @@ class BinaryTree(object):
         """
         if node is None:
             return
-        traversal_fns = (lambda: (node),
-                         lambda: self.traversal_from(node.left, order),
-                         lambda: self.traversal_from(node.right, order))
+        traversal_fns = ((node,),
+                         self.traversal_from(node.left, order),
+                         self.traversal_from(node.right, order))
         yield from it.chain.from_iterable(map(traversal_fns.__getitem__, order))
 
-    def traversal(self, order):
+    def traversal(self, order, node=None):
         """Traverse nodes depending on given order of traversal. See
         traversal_from() for details on the order of traversal.
         """
-        yield from self.traversal_from(self.root, order)
+        if node is None:
+            node = self.root
+        yield from self.traversal_from(node, order)
 
-    def top_down_traversal(self):
-        """Traverse the binary tree top-down (eg. parent then children)."""
-        yield from self.traversal((0, 1, 2))
+    def top_down_traversal(self, node=None):
+        """Traverse the binary tree top-down (eg. parent then children),
+        starting at the given node (defaults to the tree's root).
+        """
+        if node is None:
+            node = self.root
+        yield from self.traversal((0, 1, 2), node)
 
-    def bottom_up_traversal(self):
-        """Traverse the binary tree bottom-up (eg. children then parent)."""
-        yield from self.traversal((1, 2, 0))
+    def bottom_up_traversal(self, node=None):
+        """Traverse the binary tree bottom-up (eg. children then parent),
+        starting at the given node (defaults to the tree's root).
+        """
+        if node is None:
+            node = self.root
+        yield from self.traversal((1, 2, 0), node)
 
 class BinaryTreeNode(object):
-    def __init__(self, key):
+    def __init__(self, key, left=None, right=None):
         self.key = key
         self.left = None
         self.right = None
 
 class BinarySearchTree(BinaryTree):
     def search(self, key):
+        """Search for the given key in this tree, returning None if it wasn't
+        found. Works on an empty tree.
+        """
         return self.search_from(key, self.root)
 
     def search_from(self, key, node):
+        """Search recursively for the given key, starting at the given node.
+        Note that if you feel adventurous, you can give a node from another
+        tree. Returns None if the key wasn't found, works on a None node.
+        """
         if node is None:
             return None
-        elif key == node.key:
-            return node
-        elif key < node.key:
+        if key < node.key:
             return self.search(key, node.left)
-        else:
+        elif node.key < key:
             return self.search(key, node.right)
+        else:
+            return node
 
-    def in_order_traversal(self, reverse=False):
-        yield from self.traversal((1, 0, 2))
+    def in_order_traversal(self, reverse=False, node=None):
+        """Traverse the tree in the order that the keys are sorted by, starting
+        at the given node (defaults to the tree's root).
+        """
+        if node is None:
+            node = self.root
+        yield from self.traversal((2, 0, 1) if reverse else (1, 0, 2), node)
+
+    # pre-order traversal, equivalent to a top-down traversal
+    pre_order_traversal = BinaryTree.top_down_traversal
+
+    # post-order traversal, equivalent to a bottom-up traversal
+    post_order_traversal = BinaryTree.bottom_up_traversal
+
+    def insert_at(self, key, node):
+        """Insert the key recursively from the given node. Note that if you
+        feel adventurous, you can give a node from another tree. Returns the
+        current node.
+        """
+        if node is None:
+            return BinaryTreeNode(key, None, None)
+        if key < node.key:
+            node.left = self.insert_at(key, node.left)
+        else:
+            node.right = self.insert_at(key, node.right)
+        return node
 
     def insert(self, key):
-        raise NotImplementedError
+        """Insert the given key in the tree, starting at the root of course."""
+        self.root = self.insert_at(key, self.root)
+
+    def min(self, node=None):
+        """Returns the node containing the smallest key in the current tree."""
+        if node is None:
+            node = self.root
+        if node is None:
+            return None
+        return next(self.in_order_traversal())
+
+    def max(self, node=None):
+        """Returns the node containing the largest key in the current tree."""
+        if node is None:
+            node = self.root
+        if node is None:
+            return None
+        return next(self.in_order_traversal(reverse=True))
 
     def delete(self, key):
         raise NotImplementedError
